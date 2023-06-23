6eff58b6761dd3bc5b2a72614fc68014fd0a5793
Lee Hyohwak <crewe1746@naver.com>
2023-06-23 20:57:11 +0900

    Add: add page container

---
+++ frontend/src/components/container/Container.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const ContainerDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: #f5f5f5;
+  margin: 0 auto;
+`;
+
+const Container = (props) => {
+  return <ContainerDiv>{props.children}</ContainerDiv>;
+};
+
+export default Container;
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Container from "../components/container/Container";
 
 const Search = () => {
-  return <div>Search</div>;
+  return <Container>Search</Container>;
 };
 
 export default Search;
