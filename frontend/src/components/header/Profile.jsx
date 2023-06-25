b9bc90a59b95cf0d34da2fe2611bf9714c934a2e
이승민 <zeroinmars@gmail.com>
2023-06-26 00:36:12 +0900

    Update: update Header, add profile menu

--- frontend/src/components/header/Profile.jsx
+++ frontend/src/components/header/Profile.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components';
 
-const Profile = () => {
+
+const ProfileButton = styled.div`
+  &:hover {
+    div {
+        visibility: visible;
+    }
+  }
+  div {
+    visibility: hidden;
+  }
+`;
+
+const Menu = styled.div`
+    position:absolute;
+`;
+
+const SubMenu = styled.div`
+    padding:10px;
+
+`;
+
+const Profile = ({web3}) => {
+    const [balance, setBalance] = useState("");
+    useEffect(()=>{
+        web3.eth.getAccounts().then((account)=>{
+            web3.eth.getBalance(account[0])
+            .then((bal)=>{
+                return web3.utils.fromWei(bal, "ether");
+            })
+            .then(eth => {
+                eth = Number(eth).toFixed(4);
+                setBalance(eth);
+                
+            });
+        });
+    },[])
+
+
     return (
-    <div>
-        <img src="profile.png" alt="profile" width="70px" height="70px"/>
-    </div>
+        <div style={{display:"flex"}}>
+            <h3>{balance} ETH</h3>
+            <ProfileButton><img src="profile.png" alt="profile" width="70px" height="70px"/>
+                <Menu>
+                    <SubMenu>* Profile</SubMenu>
+                    <SubMenu>* Create</SubMenu>
+                    <SubMenu>* Log Out</SubMenu>
+                </Menu>
+            </ProfileButton>
+        </div>
+    
     );
     
 }
--- frontend/src/components/header/WalletConnection.jsx
+++ frontend/src/components/header/WalletConnection.jsx
@@ -14,10 +14,14 @@ const StyledButton = styled.button`
   }
 `;
 
-const WalletConnection = () => {
+const WalletConnection = ({web3, setIsLogged}) => {
+    const handleWalletConnect = () => {
+        web3.eth.requestAccounts();
+        setIsLogged(true);
+    }
   return (
     <StyledButton>
-        <h2>Connect Wallet</h2>
+        <h2 onClick={handleWalletConnect}>Connect Wallet</h2>
     </StyledButton>
   );
 };
