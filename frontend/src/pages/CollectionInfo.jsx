d60c66ee43859b5d900022d7743ca7d64b788e4b
이승민 <zeroinmars@gmail.com>
2023-06-28 14:08:35 +0900

    Fix: NFTCardContainer path fixed;

--- frontend/src/pages/CollectionInfo.jsx
+++ frontend/src/pages/CollectionInfo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ImageBanner from "../components/frame/main/collection-page-main-component/ImageBanner";
 import Description from "../components/frame/main/collection-page-main-component/Description";
-import NFTCardContainer from "../components/frame/main/collection-page-main-component/NFTCardContainerComponent";
+import NFTCardContainer from "../components/frame/main/collection-page-main-component/NFTCardContainer";
 
 const CollectionInfo = () => {
   return (
